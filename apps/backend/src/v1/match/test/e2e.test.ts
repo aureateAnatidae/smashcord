0272d796064436bc954dcf8e80e7aaa172e283c1
aureateAnatidae <34411014+aureateAnatidae@users.noreply.github.com>
2025-12-22 16:01:39 -0700

    Add TODO for e2e. Not sure how to proceed.

--- apps/backend/src/v1/match/test/e2e.test.ts
+++ apps/backend/src/v1/match/test/e2e.test.ts
@@ -1,13 +1,26 @@
+import { init_tables, init_views, teardown } from "@db/init_tables";
+import { test_knexDb } from "@test/test_knexfile";
+import app from "@v1/match/router";
 import type { MatchReport } from "@v1/match/schemas";
 import { mock_MatchReport } from "@v1/match/test/mock.schemas";
-import app from "@v1/match/router";
 import { Hono } from "hono";
 import { testClient } from "hono/testing";
-import { describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 
+
+// TODO: Unsure how to write tests here. 
+// If e2e tests should not be a superset of tested functionality on integration tests, can we prevent total overlapping?
+// How can we inject test_knexDb to the `service.ts` functions while only calling HTTP routes?
 describe("A user may report a Match", () => {
+    beforeEach(async () => {
+        await init_tables(test_knexDb);
+        await init_views(test_knexDb);
+    });
+    afterEach(async () => {
+        await teardown(test_knexDb);
+    });
     test("POST a mock MatchReport", async () => {
         const query: MatchReport = mock_MatchReport();
-        const response = await app.request("/")
+        const response = await app.request("/");
     });
 });
